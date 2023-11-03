bd91a19ae4c477949ab3782ce62ab56f43bd94c6
FelixHuiskamp <145978180+FelixHuiskamp@users.noreply.github.com>
2023-11-03 14:08:36 +0100

    Add files via upload

--- verhaaltje.js
+++ verhaaltje.js
@@ -1,23 +1,124 @@
-console.log("Once in a galaxy far far away.")
-console.log("There was a planet named 惑星 also know as wakusei.")
-console.log("In a small town there lived a jedi.")
-console.log("But not just any jedi. A jedi ronin.")
-console.log("The sith tried to hunt them down.")
-console.log("But they were smart en strong.")
-console.log("The sith lord came to the planet to kill the jedi ronin.")
-console.log("The sith lord threatened to kill the villagers for the jedi to come out of his small house.")
-console.log("A man with robes on came out of his small house.")
-console.log("The sith lord asked: Who are you?")
-console.log("Then the stranger said: I'm no one.")
-console.log("The sith lord turn her lightsaber on.")
-console.log("She said i wasn't asking.")
-console.log("She rushed towards the stranger.")
-console.log("The stranger pulled his lightsaber.")
-console.log("But it was red.")
-console.log("The sith lord says: But.. but it's  red.")
-console.log("They fought for a while. But eventually the Jedi ronin won.")
-console.log("The sith lord asked: Why is your light saber red?")
-console.log("The jedi ronin pulled a red crystal out of his pocket. I hunt sith.")
-console.log("The sith lord dies in front of him.")
-console.log("The end")
-console.log("Story based on Star Wars visions")
+const readline = require('readline-sync');
+
+console.log("Once in a galaxy far far way");
+console.log("There was a planet named 惑星 also know as wakusei.");
+console.log("In a small town there lived a jedi.");
+console.log("But not just any jedi. A jedi ronin.");
+console.log("The sith tried to hunt them down.");
+console.log("But they were smart and strong.");
+console.log("The sith lord came to the planet to kill the jedi ronin.");
+console.log("The sith lord threatened to kill the villagers for the jedi to come out of his small house.");
+let answer1 = readline.question("Do you choose to Come out of the house A: yes or B: no");
+
+if(answer1 == "A"){
+    console.log("The jedi comes out of his house and the sith doesnt kill the villagers.");
+
+} else if (answer1 == "B"){
+    console.log("The sith kills the villagers. [dead end]");
+
+}
+console.log("A man with robes came out of his small house.");
+console.log("The sith lord asked: Who are you?");
+console.log("Then the stranger said: I'm no one.");
+console.log("The sith lord turn her lightsaber on.");
+console.log("She said i wasn't asking.");
+console.log("She rushed towards the stranger.");
+let answer2 = readline.question("Do you pull your lightsaber? A: yes or B: No");
+
+if(answer1 == "A"){
+    console.log("The jedi pulls his lightsaber.");
+
+} else if (answer1 == "B"){
+    console.log("The jedi doesnt pull his lightsaber. [dead end]");
+
+}
+console.log("The stranger pulled his lightsaber.");
+console.log("But it was red.");
+console.log("But.. but it's red.");
+let answer3 = readline.question("Do you fight or defend A: fight B: Defend");
+
+if(answer1 == "A"){
+    console.log("The jedi loses so he dies. BAD ENDING");
+
+} else if (answer1 == "B"){
+    console.log("the jedi wins and successfully kills the sith.");
+
+}
+console.log("They fought for a while. But eventually the Jedi ronin won.");
+let answer4 = readline.question("Fight or die A: Slash and dodge the attack B: defend the attack");
+
+if(answer1 == "A"){
+    console.log("The jedi Slashes the opponent");
+
+} else if (answer1 == "B"){
+    console.log("The sith kills the jedi. [dead end]");
+
+}
+let answer5 = readline.question("Fight or die A: Slash and dodge the attack B: defend the attack");
+
+if(answer1 == "A"){
+    console.log("The sith dodges and kills the jedi [dead end]");
+
+} else if (answer1 == "B"){
+    console.log("The jedi defends succesfully.");
+
+}
+let answer6 = readline.question("Fight or die A: Slash and dodge the attack B: defend the attack");
+
+if(answer1 == "A"){
+    console.log("The sith dodges and kills the jedi [dead end]");
+
+} else if (answer1 == "B"){
+    console.log("The jedi defends succesfully.");
+
+}
+let answer7 = readline.question("Fight or die A: Slash and dodge the attack B: defend the attack");
+
+if(answer1 == "A"){
+    console.log("The jedi damages the opponent.");
+
+} else if (answer1 == "B"){
+    console.log("The jedi tries to dodge but fails. [dead end]");
+
+}
+let answer8 = readline.question("Fight or die A: Slash and dodge the attack B: defend the attack");
+
+if(answer1 == "A"){
+    console.log("The jedi damages the opponent.");
+
+} else if (answer1 == "B"){
+    console.log("The jedi tries to block but fails. [dead end]");
+
+}
+let answer9 = readline.question("Fight or die A: Slash and attack B: dodge the attack");
+
+if(answer1 == "A"){
+    console.log("The jedi damages the opponent but fails [dead end].");
+
+} else if (answer1 == "B"){
+    console.log("The jedi dodges the attack.");
+
+}
+let answer10 = readline.question("Fight or die A: Slash and dodge the attack B: defend the attack");
+
+if(answer1 == "A"){
+    console.log("The jedi damages the opponent.");
+
+} else if (answer1 == "B"){
+    console.log("The jedi tries to dodge but fails. [dead end]");
+
+}
+let answer11 = readline.question("Fight or die A: attack B: defend");
+
+if(answer1 == "A"){
+    console.log("The jedi dodges and stabs the sith. [GOOD ENDING]");
+
+} else if (answer1 == "B"){
+    console.log("The jedi tries to dodge but fails. [dead end]");
+
+}
+console.log("The sith lord asked: Why is your light saber red?");
+console.log("The jedi ronin pulled a red crystal out of his pocket. I hunt sith.");
+console.log("The sith lord dies in front of him.");
+console.log("The end");
+console.log("Story based on Star Wars Visions");
